3e671c3acb14f9d4be9bb507f7278585f7d69b97
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-03-06 14:06:32 -0500

    Add start of recusion lectures

---
+++ basics/factorial.cpp
@@ -0,0 +1,64 @@
+/**
+ *  @file factorial.cpp
+ *
+ *  Show recursion
+ *
+ *  @author Michael John Decker <mdecker6@kent.edu>
+ */
+
+#include <iostream>
+
+
+/**
+ * Recursion - see Recursion
+ * 
+ * There are three essenstial parts to qualify as recursion
+ * * At least 1 base case
+ * * A recursive case, either directly (function calls itself)
+ *   or indirectly (a sequence of calls eventially calls this function)
+ * * The recursive case must make progress towards the base case. That is,
+ *   recursion must end, and not be infinite.
+ * 
+ */
+
+/**
+ * factorial
+ * @param num factorial to compute (i.e., num!)
+ *
+ * Compute factorial of num (i.e., num!) using regular recursion.
+ *
+ * @returns num!
+ */
+unsigned long long factorial(int num) {
+    // base case
+    if(num <= 1) return 1;
+
+    // recursive case
+    return num * factorial(num - 1);
+
+}
+
+
+unsigned long long bad_factorial(int num) {
+    // base case
+    if(num <= 1) return 1;
+
+    // infinitely recursive
+    return num * factorial(num);
+
+}
+
+
+int main(int argc, char * argv[]) {
+
+    std::cout << "factorial(0):       " << factorial(0)  << '\n';
+    std::cout << "factorial(1):       " << factorial(1)  << '\n';
+    std::cout << "factorial(5):       " << factorial(5)  << '\n';
+    std::cout << "factorial(20):      " << factorial(20) << '\n';
+
+
+    // Uncomment this to see what happens with infinite recusion
+    std::cout << "bad_factorial(20):  " << bad_factorial(20) << '\n';
+
+    return 0;
+}
