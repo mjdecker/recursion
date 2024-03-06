01412867aee472e79574c1a84aac2f7048bd6533
Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
2024-03-06 15:49:31 -0500

    Add bug lab

--- debugging/pop_growth.cpp
+++ debugging/pop_growth.cpp
@@ -1,3 +1,53 @@
+/**
+ *  @file towers.cpp
+ *
+ *  Recurively solve towers of hanoi
+ *
+ *  @author Michael J. Decker, Ph.D. <mdecke@bgsu.edu>
+ *  @author Basma Alqadi, Ph.D. <balqadi@kent.edu>
+ *  @author Jonathan I. Maletic, Ph.D. <jmaletic@kent.edu>
+ */
+
+/**
+  * The following is a program with several common bugs for you to fix.
+  * Use output message (std::cerr) to help you debug.
+  * 
+  * An example run should look like this.
+
+Enter the name of the university
+BGSU
+What is the current population? 100
+What is the rate of growth? (e.g., for 10% enter 10) 10
+
+Year  New Population
+0   110
+1   121
+2   133
+3   146
+4   160
+5   176
+
+Final population of bgsu is 176
+Done? (Y/N) N
+Starting again
+------------------------------
+What is the current population? 1000
+What is the rate of growth? (e.g., for 10% enter 10) 5
+
+Year  New Population
+0   1050
+1   1102
+2   1157
+3   1214
+4   1274
+5   1337
+
+Final population of bgsu is 1337
+Done? (Y/N) Y
+Exiting...
+
+*/
+
 #include <iostream>
 
 const int STRING_SIZE = 30;
@@ -46,15 +96,15 @@ int main(int argc, const char * argv[]) {
     std::cin >> done;
     switch (toupper(done)) {
       case 'Y':
-	std::cout << "Exiting...\n";
+        std::cout << "Exiting...\n";
       case 'N':
-	std::cout << "Starting again\n";
-	std::cout << "------------------------------\n";
+        std::cout << "Starting again\n";
+        std::cout << "------------------------------\n";
       default:
-	std::cout << argv[2] << ": Invalid input\n";
-	std::cout << "The program will exit\n";
-	std::cout << "------------------------------\n";
-	return -1;
+        std::cout << argv[2] << ": Invalid input\n";
+        std::cout << "The program will exit\n";
+        std::cout << "------------------------------\n";
+        return -1;
       }
   }
   return 0;
